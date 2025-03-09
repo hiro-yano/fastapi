73ad78da8a1850a904733534e6e2e248003d4399
hiro-yano <hyano1991@gmail.com>
2025-03-09 17:41:20 +0900

    remove start

--- main.py
+++ main.py
@@ -80,10 +80,4 @@ def process_video(request: VideoRequest):
         raise HTTPException(status_code=500, detail=str(e))
     
 
-def start():
-    import uvicorn
-    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
 
-
-if __name__ == "__main__":
-    start()
